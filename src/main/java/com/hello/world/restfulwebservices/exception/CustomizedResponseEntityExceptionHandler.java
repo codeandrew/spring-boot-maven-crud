e7ce1ece20047a9c1e9f76af419a15db2efdedd8
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-01-04 16:01:50 +0800

    Create Validation Exception Handler

--- src/main/java/com/hello/world/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/hello/world/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.hello.world.restfulwebservices.exception;
 
 import com.hello.world.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +38,16 @@ extends ResponseEntityExceptionHandler {
                         request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+    HttpHeaders headers, HttpStatus status, WebRequest request){
+
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(),
+                        ex.getBindingResult().toString() );
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
